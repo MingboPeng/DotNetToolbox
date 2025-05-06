5dd0006230688e92e9fcba08bbb4b3f908d75e54
Mingbo Peng <mingo1214@gmail.com>
2025-05-06 19:43:20 -0400

    fix(Service): use compiled source.dll if exists

--- SchemaGenerator/GenService.cs
+++ SchemaGenerator/GenService.cs
@@ -33,7 +33,7 @@ public class GenService : GenProcessorBase
             // generate TS services and dtos
             GenTsService.Execute(services, dtos, _sourceDoc);
         }
-    
+
     }
 
     private static System.Xml.Linq.XDocument _sourceDoc;
@@ -47,59 +47,80 @@ public class GenService : GenProcessorBase
 
     private static List<Type> LoadSourceCode(string sourceDir)
     {
-        Console.WriteLine($"Loading/compiling source code from: {sourceDir}");
-        var csFiles = System.IO.Directory.GetFiles(sourceDir, "*.cs", SearchOption.AllDirectories);
-  
-        var syntaxTrees = csFiles.Select(_ => CSharpSyntaxTree.ParseText(File.ReadAllText(_))).ToList();
-
-        var references = new[]
+        // get all package dlls
+        var dllFiles = System.IO.Directory.GetFiles($"{sourceDir}/bin", "*.dll", SearchOption.AllDirectories).ToList();
+        var sourceDll = dllFiles?.FirstOrDefault(x => System.IO.Path.GetFileName(x) == "source.dll");
+        if (!string.IsNullOrEmpty(sourceDll))
+            dllFiles = dllFiles.Where(_ => _ != sourceDll).ToList();
+        var sourceAssembly = Assembly.LoadFrom(sourceDll);
+        foreach (var item in dllFiles)
         {
+            Assembly.LoadFrom(item);
+        }
+
+        // get xml doc file path
+        string xmlDocFilePath = System.IO.Path.ChangeExtension(sourceDll, "xml");
+
+
+        if (sourceAssembly == null)
+        {
+            Console.WriteLine($"Loading/compiling source code from: {sourceDir}");
+            var csFiles = System.IO.Directory.GetFiles(sourceDir, "*.cs", SearchOption.AllDirectories);
+
+
+            var syntaxTrees = csFiles.Select(_ => CSharpSyntaxTree.ParseText(File.ReadAllText(_))).ToList();
+
+            var references = new[]
+            {
              MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // System.dll
              MetadataReference.CreateFromFile(typeof(RequiredAttribute).Assembly.Location), // System.ComponentModel.DataAnnotations.dll
              MetadataReference.CreateFromFile(typeof(Attribute).Assembly.Location), // System.Runtime.dll
              MetadataReference.CreateFromFile( System.IO.Path.Combine(System.IO.Path.GetDirectoryName(typeof(object).Assembly.Location), "System.Runtime.dll")) // System.Runtime.dll
-        };
+        }.ToList();
+            //references.AddRange(dllFiles.Select(_ => MetadataReference.CreateFromFile(_)));
 
 
-        var compilation = CSharpCompilation.Create("DynamicAssembly")
-            .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
-            .AddReferences(references)
-            .AddSyntaxTrees(syntaxTrees);
+            var compilation = CSharpCompilation.Create("DynamicAssembly")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+                .AddReferences(references)
+                .AddSyntaxTrees(syntaxTrees);
 
-        using var ms = new MemoryStream();
-        using var xmlStream = new MemoryStream(); // XML documentation output
+            using var ms = new MemoryStream();
+            using var xmlStream = new MemoryStream(); // XML documentation output
 
-        EmitResult result = compilation.Emit(ms, xmlDocumentationStream: xmlStream);
+            EmitResult result = compilation.Emit(ms, xmlDocumentationStream: xmlStream);
 
-        if (!result.Success)
-        {
-            foreach (var err in result.Diagnostics)
+            if (!result.Success)
             {
-                Console.WriteLine(err);
+                foreach (var err in result.Diagnostics)
+                {
+                    Console.WriteLine(err);
+                }
+
+                throw new Exception("LoadServiceSource: Compilation failed.");
             }
 
-            throw new Exception("LoadServiceSource: Compilation failed.");
+            ms.Seek(0, SeekOrigin.Begin);
+            sourceAssembly = Assembly.Load(ms.ToArray());
+
+            // get doc
+            xmlStream.Seek(0, SeekOrigin.Begin);
+            string xmlDocumentation = new StreamReader(xmlStream).ReadToEnd();
+
+            var xmlDocFile = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "sourceDoc.xml");
+            if (System.IO.File.Exists(xmlDocFile))
+                System.IO.File.Delete(xmlDocFile);
+            File.WriteAllText(xmlDocFile, xmlDocumentation);
+            xmlDocFilePath = xmlDocFile;
+
+            Console.WriteLine("Compilation completed!");
         }
+       
+        var types = sourceAssembly.GetTypes().ToList();
 
-        ms.Seek(0, SeekOrigin.Begin);
-        Assembly assembly = Assembly.Load(ms.ToArray());
-        var types = assembly.GetTypes().ToList();
-
-
-        // get doc
-        xmlStream.Seek(0, SeekOrigin.Begin);
-        string xmlDocumentation = new StreamReader(xmlStream).ReadToEnd();
-
-        var xmlDocFile = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "sourceDoc.xml");
-        if (System.IO.File.Exists(xmlDocFile))
-            System.IO.File.Delete(xmlDocFile);
-        File.WriteAllText(xmlDocFile, xmlDocumentation);
-
-
-        var xmlDoc = System.Xml.Linq.XDocument.Load(xmlDocFile);
+        var xmlDoc = System.Xml.Linq.XDocument.Load(xmlDocFilePath);
         _sourceDoc = xmlDoc;
 
-        Console.WriteLine("Compilation completed!");
         return types;
 
     }
